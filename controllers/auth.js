0204345cb290a8192171b24d674d092c85ccb5e5
Stelin <stelinduarte100@gmail.com>
2021-07-29 18:22:51 -0600

    :sparkles: Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 const { generateJWT } = require("../helpers/generate-jwt");
+const { googleValidation } = require("../helpers/google-validation");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -33,6 +34,32 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignin = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { email, name, img } = await googleValidation(id_token);
+    let user = await User.findOne({ email });
+    if (!user) {
+      //Tenemos que crearlo
+      const data = { name, email, password: ":p", img, google: true };
+      user = new User(data);
+      await user.save();
+    }
+
+    //Si el usuario en DB  tiene el estado en false
+    if (!user.state) {
+      res.status(401).json({ msg: "User deleted" });
+    }
+    //Generar el jwt
+    const token = await generateJWT(user.id);
+
+    res.json({ msg: "Todo ok! google signin", user, token });
+  } catch (error) {
+    res.status(400).json({ msg: "Token is not valid" });
+  }
+};
 module.exports = {
   login,
+  googleSignin,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth");
+const { login, googleSignin } = require("../controllers/auth");
 const { validateFields } = require("../middlewares/validate-fields");
 
 const router = Router();
@@ -12,6 +12,12 @@ router.post(
     validateFields,
   ],
   login
+);
+
+router.post(
+  "/google",
+  [check("id_token", "id_token is required").not().isEmpty(), validateFields],
+  googleSignin
 );
 
 module.exports = router;
